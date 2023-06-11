08d3814d768f2bf402ebb217d1daddb7988562cf
Daniel Atanasovski <daniel@danielatanasovski.com>
2023-06-11 15:46:37 +0000

    More renamings

--- pod-labels-webhook/.eslintrc.cjs
+++ pod-labels-webhook/.eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
-    tsconfigRootDir: `${__dirname}/pod_labels_webhook`,
+    tsconfigRootDir: `${__dirname}/pod-labels-webhook`,
   },
   root: false,
 };
